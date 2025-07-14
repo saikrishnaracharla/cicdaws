84816503d9cb357dd2c87657ad3f887f2c4e8468
Racharla saikrishna <115521007+saikrishnaracharla@users.noreply.github.com>
2025-07-14 11:41:19 +0530

    update1

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("cicd");
 });
-app.listen(3050,()=>{
-    console.log("Server is running on port 3050");
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
 });
